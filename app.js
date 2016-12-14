87d97250dcf34fda5db380363f6a6f68a843f4f6
Nitin Pradeep Kumar <getnpk@gmail.com>
2016-12-14 11:23:08 +0000

    initial User setup for passport

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 var express = require("express"),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    bodyParser = require("body-parser"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose"),
+    User = require("./models/user");
 
 mongoose.connect("mongodb://localhost/passport_authentication");
 
 var app = express();
-
 app.set("view engine", "ejs");
 
+// passport
+app.use(require("express-session")({
+    secret: "Batman Returns",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// routes
 app.get("/", function(req, res){
    res.render("home"); 
 });
