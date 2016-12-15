653edf21c56de2fb835f2de66f93f52eb7d1c0cb
Nitin Pradeep Kumar <getnpk@gmail.com>
2016-12-15 06:28:59 +0000

    passportLocalMongoose only for User model

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require("express"),
     passport = require("passport"),
     bodyParser = require("body-parser"),
     LocalStrategy = require("passport-local"),
-    passportLocalMongoose = require("passport-local-mongoose"),
     User = require("./models/user");
 
 mongoose.connect("mongodb://localhost/passport_authentication");
