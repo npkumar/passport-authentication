f480a79e178038da69be7b1d669b040193b47fa2
Nitin Pradeep Kumar <getnpk@gmail.com>
2016-12-15 06:24:39 +0000

    isAuthenticated middle and add to secret route

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.get("/", function(req, res){
    res.render("home"); 
 });
 
-app.get("/secret", function(req, res){
+app.get("/secret", isLoggedIn, function(req, res){
    res.render("secret"); 
 });
 
@@ -64,6 +64,13 @@ app.get("/logout", function(req, res) {
     req.logout();
     res.redirect("/");
 });
+
+function isLoggedIn(req, res, next){
+    if (req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("Server running"); 
